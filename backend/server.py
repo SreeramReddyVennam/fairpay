5c842738e297553d2c60ffd9aedaafdeefb24b5b
herocharge <techwithbhargav@gmail.com>
2022-06-19 01:09:13 +0530

    [UPDATE] added get request capability for menus

--- backend/server.py
+++ backend/server.py
@@ -1,8 +1,20 @@
+import json as jsn
 from flask import *
+from pymongo import MongoClient
+
 app = Flask(__name__)
+client = MongoClient('mongodb+srv://herocharge:herocharge@cluster0.ly1r8.mongodb.net/?retryWrites=true&w=majority', 27017, username='herocharge', password='herocharge')
+
+db = client.fairplay
 
 @app.route('/resources/menu/<shop>')
 def menu(shop):
+    shop_menu = db[shop+"_menu"]
+    results = shop_menu.find()
+    res = []
+    for r in results:
+        res.append(str(r))
+    return str(res)
     None
 
 @app.route('/')
